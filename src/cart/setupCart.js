9de9503b7adbe26ec67ad2fc1d2ffa95c0c77420
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-07-30 11:16:39 +0200

    addToCart done

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,7 +10,48 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
+
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the cart
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM
+    addToCartDOM(product);
+    console.log(cart);
+  } else {
+    // update values
+  }
+  // add one to the item count
+  displayCartItemCount();
+  // display cart totals
+  displayCartTotal();
+  //set cart in local storage
+  setStorageItem('cart', cart);
+
+  // more stuff coming up
   openCart();
 };
+
+const displayCartItemCount = () => {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+};
+const displayCartTotal = () => {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)}`;
+};
+
+const init = () => {};
+init();
